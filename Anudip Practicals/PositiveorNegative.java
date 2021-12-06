92c0ff2cac6eb5fbf44e3b768d12afcecd5f7955
Akshay0604 <95092127+Akshay0604@users.noreply.github.com>
2021-12-06 22:18:33 +0530

    Create PositiveorNegative.java

---
+++ Anudip Practicals/PositiveorNegative.java
@@ -0,0 +1,17 @@
+public class PositiveorNegative{
+	public static void main(String args[]){
+	 int a = -9;
+	 if(a>=0){
+	  System.out.println("Number is Positive");
+	  }
+	  else{
+	  System.out.println("Number is Negative");
+	  }
+	 if(a<=0){
+	  System.out.println("Number is Negative");
+	  }
+	  else{
+	  System.out.println("Number is Positive");
+	 }
+	}
+}
